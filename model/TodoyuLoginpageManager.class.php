bf102616ae77a9f550eef5a91edcf2dde858aafe
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-01-13 16:02:31 +0000

    * Removed Todoyu::getVersionData() * Updated TodoyuDatabase::getTables() mysql_list_tables was deprecated * Implemented clean version update sql handling * Removed version variabled, use constants in template * Fixed link to bugtracker * Fixed task order in portal (only subgroups were sorted) * Removed functionless link for taskuser

--- model/TodoyuLoginpageManager.class.php
+++ model/TodoyuLoginpageManager.class.php
@@ -36,7 +36,7 @@ class TodoyuLoginpageManager {
 		TodoyuFrontend::addMenuEntry('login', 'LLL:loginpage.logintab.login', '?ext=loginpage&controller=ext', 10);
 		TodoyuFrontend::addMenuEntry('help', 'LLL:loginpage.logintab.help', 'http://developer.todoyu.com', 20);
 		TodoyuFrontend::addMenuEntry('blog', 'LLL:loginpage.logintab.blog', 'http://blog.snowflake.ch/de/blog-post/2009/01/31/todoyu-gewinnt-osbf-award/', 30);
-		TodoyuFrontend::addMenuEntry('feedback', 'LLL:loginpage.logintab.feedback', 'http://feedback.todoyu.com/', 100);
+		TodoyuFrontend::addMenuEntry('feedback', 'LLL:loginpage.logintab.feedback', 'http://bugs.todoyu.com/', 100);
 	}
 
 
