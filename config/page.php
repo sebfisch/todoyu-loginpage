915550f838adf9af7b61d705194fc293aaf66bf3
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-03-12 14:22:46 +0000

    * Headlets * Added profile Headlet.js * Removed todoyu tab

--- config/page.php
+++ config/page.php
@@ -1,10 +1,5 @@
 <?php
 
-if( TodoyuAuth::isLoggedIn() ) {
-
-	TodoyuFrontend::addSubmenuEntry('todoyu', 'logout', 'LLL:loginpage.submenu.logout', 'javascript:Todoyu.Ext.loginpage.logout()', 1000);
-}
-
 TodoyuHeadManager::addHeadlet('TodoyuHeadletLogout', 10);
 
 ?>
