110569449485210af569e5a80738ba5bd92d4937
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-04-20 16:23:51 +0000

    * Replaced time rouding function with more powerful one (TodoyuTime::getRoundedTime()) * Fixed formvalidator requiredIfNotEmpty. Added except fields * Calendar refreshes if a new event is added over quickcreate while being in the calendar view * Added some better type docs for js * Use rounded time for quickcreate events (instead of exact current minute) * Fixed rights check for contact quick create * Changed tag name field to fields for validator requiredIfNotEmpty * Added company label for contact rights * Cleaned up company visit card * Added hook for reinitialization on quicktask reload after invalid entries are saved * Added missing action attribute for quicktask form * Limited date_start and date_end to internal persons

--- assets/js/PanelWidgetLoginNews.js
+++ assets/js/PanelWidgetLoginNews.js
@@ -1,5 +1,35 @@
+/****************************************************************************
+* todoyu is published under the BSD License:
+* http://www.opensource.org/licenses/bsd-license.php
+*
+* Copyright (c) 2010, snowflake productions gmbh
+* All rights reserved.
+*
+* This script is part of the todoyu project.
+* The todoyu project is free software; you can redistribute it and/or modify
+* it under the terms of the BSD License.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD License
+* for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script.
+*****************************************************************************/
+
+/**
+ * Loginnews Panelwidget
+ * Load news from todoyu website to a panelwidget displayed at the loginscreen
+ *
+ * @package		Todoyu
+ * @subpackage	Loginpage
+ */
 Todoyu.Ext.loginpage.PanelWidget.LoginNews = {
-	
+
+	/**
+	 * Handler when news is loaded
+	 * Show iframe content instead of static news dummy
+	 */
 	newsLoaded: function() {
 		var live	= $('news-live');
 		var iframe	= $('loginnews-iframe');
