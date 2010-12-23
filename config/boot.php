227521da93380bd2df9f0c0c09d3c6a158c266f1
nkarrer <nkarrer@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-12-23 12:55:13 +0000

    * changed URL of cookies and JavaScript manuals

--- config/boot.php
+++ config/boot.php
@@ -38,7 +38,7 @@ Todoyu::$CONFIG['AUTH']['login'] = array(
 
 Todoyu::$CONFIG['EXT']['loginpage']['waitAtFailLogin'] = 2;
 
-Todoyu::$CONFIG['EXT']['loginpage']['manuallinks']['cookies']		= 'http://www.todoyu.com/index.php?id=347';
-Todoyu::$CONFIG['EXT']['loginpage']['manuallinks']['javascript']	= 'http://www.todoyu.com/index.php?id=346';
+Todoyu::$CONFIG['EXT']['loginpage']['manuallinks']['cookies']		= 'http://docu.todoyu.com/index.php?id=manual-cookies';
+Todoyu::$CONFIG['EXT']['loginpage']['manuallinks']['javascript']	= 'http://docu.todoyu.com/index.php?id=manual-javascript';
 
 ?>
