f7b22f683e6315cadb04a90fc49602940e524660
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2011-04-18 12:15:30 +0000

    * cleanups

--- model/TodoyuLoginpagePanelWidgetManager.class.php
+++ model/TodoyuLoginpagePanelWidgetManager.class.php
@@ -78,7 +78,7 @@ class TodoyuLoginpagePanelWidgetManager {
 		$file = PATH_CACHE.'/output/loginnews.html';
 
 		// get content between the body tags
-		$content = substr($content, strpos($content, '<body>')+6);
+		$content = substr($content, strpos($content, '<body>') + 6);
 		$content = substr($content, 0, strlen($content) - (strlen($content)-strpos($content, '</body>')));
 
 		TodoyuFileManager::makeDirDeep(PATH_CACHE.'/output');
